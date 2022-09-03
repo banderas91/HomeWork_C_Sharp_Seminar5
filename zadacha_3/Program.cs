20ec1f4a182157bfa04aa82c3380adf23b2bd6bb
K_A_A <banderas91@mail.ru>
2022-09-03 21:08:03 +0300

    задача_3

---
+++ zadacha_3/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+Console.Write("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+Console.Write(" разница  в массиве -" );
+
+
+for (int i =0; i< N; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+Console.Write( " "+(array[i]) );
+}
+
+
+int max = array[0];
+int min = array[0];
+
+for (int i = 0; i < N; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine("  между Максимальным "+ max + " и Минимальным  "+ min + " равна "+ (max - min) );
